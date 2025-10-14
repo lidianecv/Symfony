0ab5d6b8e1e77f94d28ddbfcd9796757b3ca3a16
Lidiane Nunes <lidiane2002nunes@gmail.com>
2025-10-14 16:15:29 -0100

    Controller and routes

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+
+
+class HelloController extends AbstractController {
+    public function index():Response{
+        return new Response ('<h1>Hello World!</h1>');
+    }
+
+    public function helloname($name):Response{
+        return new Response ('<h1>Hello ' . $name . '</h1>');
+
+}
+
+
+}
